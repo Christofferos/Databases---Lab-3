bbdfb3780ed20c4e622197471e31ad9b9604151b
ErikChHenriksson <erik.ch.henriksson@gmail.com>
2019-11-21 13:26:15 +0100

    Connection success

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 //Here is the information the needed for connection
 $host = "nestor2.csc.kth.se"; //Don't change
-$db_name = "XXX"; //Your username. If you want to look at a simple example for the main page, connect to "aliceer". Delete and create not available for this database.
-$username = "XXX"; //Your username
-$password = "XXXX"; //The password you retrieved from your folder in Putty
+$db_name = "kriwer"; //Your username. If you want to look at a simple example for the main page, connect to "aliceer". Delete and create not available for this database.
+$username = "kriwer"; //Your username
+$password = "IcqrcdYorEet"; //The password you retrieved from your folder in Putty
 
 try {
     $con = new PDO("pgsql:host={$host};dbname={$db_name}", $username, $password);
